d5fc46603bff4f5a7a5e1dcc8a9a1fd46f24922a
uki00a <uki00a@gmail.com>
2020-01-02 15:11:57 +0900

    update: initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# deno-init
+
+## Usage
+
+```sh
+$ deno install deno-init https://denopkg.com/uki00a/deno-init/cmd.ts --allow-env --allow-read --allow-write
+$ deno-init
+```
---
+++ cmd.ts
@@ -0,0 +1,68 @@
+import { basename, join, dirname } from 'https://deno.land/std@v0.27.0/path/mod.ts';
+import { writeFileStr } from 'https://deno.land/std@v0.27.0/fs/write_file_str.ts';
+import { copy } from 'https://deno.land/std@v0.27.0/fs/copy.ts';
+
+function createTsConfig(denoDir: string): object {
+  return {
+    "compilerOptions": {
+      "target": "esnext",
+      "module": "esnext",
+      "baseUrl": ".",
+      "paths": {
+        "https://*": [
+          `${denoDir}/deps/https/*`
+        ],
+        "http://*": [
+          `${denoDir}/deps/http/*`
+        ]
+      },
+     "plugins": [{ "name": "typescript-deno-plugin" }]
+    }
+  };
+}
+
+function createPackage(name: string, tsVersion: string): object {
+  return {
+    "name": name,
+    "version": "0.0.1",
+    "license": "MIT",
+    "private": true,
+    "devDependencies": {
+      "typescript": tsVersion,
+      "typescript-deno-plugin": "latest"
+    }
+  };
+}
+
+async function prepareGitIgnore(): Promise<void> {
+  const filename = '.gitignore';
+  const src = join(Deno.cwd(), filename);
+  const dest = join(dirname(new URL(import.meta.url).pathname), filename);
+  if (src !== dest) {
+    await copy(src, dest);
+  }
+}
+
+async function writeFileJson(path: string, json: object): Promise<void> {
+  await writeFileStr(path, JSON.stringify(json, null, 2));
+}
+
+async function prepareTsConfig(): Promise<void> {
+  const denoDir = join(Deno.dir('cache'), 'deno');
+  const tsConfig = createTsConfig(denoDir);
+  await writeFileJson(join(Deno.cwd(), 'tsconfig.json'), tsConfig);
+}
+
+async function preparePackageJson(): Promise<void> {
+  const projectName = basename(Deno.cwd());
+  const pkg = createPackage(projectName, Deno.version.typescript);
+  await writeFileJson(join(Deno.cwd(), 'package.json'), pkg);
+}
+
+async function main(): Promise<void> {
+  await prepareGitIgnore();
+  await prepareTsConfig();
+  await preparePackageJson();
+}
+
+main();
